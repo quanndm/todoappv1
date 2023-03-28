0f4f99942319b659e9f44d97c429c59c520dce35
quanndm <minhquan1852@gmail.com>
2023-03-28 11:08:26 +0700

    change url api

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,8 @@ import { store } from "./store/store";
 import "./styles/index.scss";
 import axios from "axios"
 import 'react-toastify/dist/ReactToastify.css';
-const url = "http://localhost:5111"
+// const url = "http://localhost:5111"
+const url = "https://quan-todoapiv1-dev.azurewebsites.net/"
 axios.defaults.baseURL = `${url}/api`
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
